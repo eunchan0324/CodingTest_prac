4b6bc870e010506f07c35a0cbacc1e06e4eb396f
신은찬 <156327217+eunchan0324@users.noreply.github.com>
2026-02-04 14:56:07 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 74.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181942. 문자열 섞기/README.md
+++ Java/프로그래머스/0/181942. 문자열 섞기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 섞기 - 181942 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181942?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181942) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 13.15 ms
+메모리: 74.9 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 04일 14:50:01
+2026년 02월 04일 14:56:07
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,24 +1,12 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         
-        char[] s1 = new char[str1.length()];
-        char[] s2 = new char[str2.length()];
-        
-        
-        for(int i=0; i<str1.length(); i++) {
-            s1[i] = str1.charAt(i);
+        for (int i=0; i<str1.length(); i++) {
+            sb.append(str1.charAt(i));
+            sb.append(str2.charAt(i));
         }
         
-        for(int i=0; i<str2.length(); i++) {
-            s2[i] = str2.charAt(i);
-        }
-        
-        for(int i=0; i<s1.length; i++) {
-            answer += s1[i];
-            answer += s2[i];
-        }
-        
-        return answer;
+        return sb.toString();
     }
 }
