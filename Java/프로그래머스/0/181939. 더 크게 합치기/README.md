992d76bc272f9a690d99c0b906c2b688357a4334
신은찬 <156327217+eunchan0324@users.noreply.github.com>
2026-02-04 15:21:08 +0900

    [level 0] Title: 더 크게 합치기, Time: 2.30 ms, Memory: 84.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181939. 더 크게 합치기/README.md
+++ Java/프로그래머스/0/181939. 더 크게 합치기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 더 크게 합치기 - 181939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939) 
 
 ### 성능 요약
 
-메모리: 91.8 MB, 시간: 2.99 ms
+메모리: 84.8 MB, 시간: 2.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 04일 15:16:03
+2026년 02월 04일 15:21:08
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -4,6 +4,9 @@ class Solution {
         String r1 = String.valueOf(a) + String.valueOf(b);
         String r2 = String.valueOf(b) + String.valueOf(a);
     
-        return Integer.parseInt(r1) > Integer.parseInt(r2) ? Integer.parseInt(r1) : Integer.parseInt(r2);
+        int num1 = Integer.parseInt(r1);
+        int num2 = Integer.parseInt(r2);
+        
+        return Math.max(num1, num2);
     }
 }
