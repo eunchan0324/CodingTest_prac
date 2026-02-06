e28aedd4f6fbe02c5d189ae40486dc53e518b689
신은찬 <156327217+eunchan0324@users.noreply.github.com>
2026-02-06 17:01:44 +0900

    [level 0] Title: 코드 처리하기, Time: 420.65 ms, Memory: 373 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(String code) {
+
+        int mode = 0;
+        String ret = "";
+        
+        for (int idx=0; idx<code.length(); idx++) {
+            if (mode == 0) {
+                if (code.charAt(idx) != '1') {
+                    if (idx % 2 == 0) {
+                        ret += code.charAt(idx);
+                    }
+                }
+                else if (code.charAt(idx) == '1') {
+                    mode = 1;
+                }
+            }
+            
+            else {
+                if (code.charAt(idx) != '1') {
+                    if (idx % 2 != 0) {
+                        ret += code.charAt(idx);
+                    }
+                } else {
+                    mode = 0;
+                }
+            } 
+        }
+        
+        return ret.isEmpty() ? "EMPTY" : ret;
+    }
+}
