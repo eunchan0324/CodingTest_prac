b3cc06cfe2b0c0af18d8b7ba90bf2acd23491076
신은찬 <156327217+eunchan0324@users.noreply.github.com>
2026-02-07 22:06:07 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.76 ms, Memory: 96.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ Java/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,18 +1,18 @@
 class Solution {
     public String solution(int[] numLog) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         
         for (int i=1; i<numLog.length; i++) {
             int num = numLog[i]-numLog[i-1];
             
             switch(num) {
-                case 1 : answer += "w"; break;
-                case -1 : answer += "s"; break;
-                case 10 : answer += "d"; break;
-                case -10 : answer += "a"; break;
+                case 1 : sb.append("w"); break;
+                case -1 : sb.append("s"); break;
+                case 10 : sb.append("d"); break;
+                case -10 : sb.append("a"); break;
             }
         }
         
-        return answer;
+        return sb.toString();
     }
 }
