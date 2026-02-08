0be84daf89b2cdc53e51903dee67598dd49ce2e6
신은찬 <156327217+eunchan0324@users.noreply.github.com>
2026-02-09 00:27:15 +0900

    [level 0] Title: 배열 만들기 2, Time: 44.50 ms, Memory: 121 MB -BaekjoonHub

--- Java/프로그래머스/0/181921. 배열 만들기 2/README.md
+++ Java/프로그래머스/0/181921. 배열 만들기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 2 - 181921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921) 
 
 ### 성능 요약
 
-메모리: 107 MB, 시간: 89.30 ms
+메모리: 121 MB, 시간: 44.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 09일 00:05:20
+2026년 02월 09일 00:27:16
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ Java/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -3,40 +3,34 @@ import java.util.*;
 class Solution {
     public int[] solution(int l, int r) {
         ArrayList<Integer> list = new ArrayList<>();
-        
+
         for (int i=l; i<=r; i++) {
-            String num = String.valueOf(i);
-            
-            int cnt = 0;
-            
-            for (int j=0; j<num.length(); j++) {
-                char c = num.charAt(j);
-                
-                if (c == '5' || c == '0') {
-                    cnt += 1;
-                }
+            if (check(i)) {
+                list.add(i);
             }
+        }
             
-            if (cnt == num.length()) {
-                    list.add(Integer.parseInt(num));
-            }
-            
+        if (list.size() == 0) {
+            return new int[] {-1};
         }
         
         int[] answer = new int[list.size()];
-        
         for (int i=0; i<list.size(); i++) {
             answer[i] = list.get(i);
         }
+
+        return answer;
+    }
+    
+    private boolean check(int num) {
+        String numStr = String.valueOf(num);
         
-        System.out.println(list.size());
-        
-        int[] no = {-1};
-        
-        if (list.size() == 0) {
-            return no;
-        } else {
-            return answer;
+        for (char c : numStr.toCharArray()) {
+            if (c != '5' && c != '0') {
+                return false;
+            }
         }
+        
+        return true;
     }
 }
