8070e2e02e1af175c6452343c746acabdda1c69b
신은찬 <156327217+eunchan0324@users.noreply.github.com>
2026-03-02 13:16:08 +0900

    [level 0] Title: 문자열로 변환, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int n) {
+        // String answer = n + "";
+        // String answer = Integer.toString(n);
+        String answer = String.valueOf(n);
+        
+        return answer;
+    }
+}
