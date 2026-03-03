486729a7f06e5e1b574500f50fef317cd498c74d
신은찬 <156327217+eunchan0324@users.noreply.github.com>
2026-03-03 15:04:37 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        
+        for (int num : num_list) {
+            if (num == n) {
+                return 1;
+            }
+        }
+        
+        return answer;
+    }
+}
