37fd71e0fa994299de7ae4769b4d38139522c491
신은찬 <156327217+eunchan0324@users.noreply.github.com>
2026-03-04 22:21:32 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 12.02 ms, Memory: 166 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        return Arrays.stream(arr)
+            .map(n -> k % 2 == 0 ? n+k : n*k)
+            .toArray();
+    }
+}
