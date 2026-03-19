6aba303a80b4dd2d1c3e3739d2ca228dc38b9414
신은찬 <156327217+eunchan0324@users.noreply.github.com>
2026-03-19 13:52:37 +0900

    [level 0] Title: 가까운 수, Time: 0.40 ms, Memory: 85.7 MB -BaekjoonHub

--- Java/프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ Java/프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -3,25 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(int[] array, int n) {
         Arrays.sort(array);
+        
         int[] answer = new int[array.length];
         
         for (int i=0; i<array.length; i++) {
-            answer[i] = array[i]-n;
+            answer[i] = Math.abs(array[i]-n);
         }
         
-        int min = 100;
+        int min = answer[0];
+        
         for (int num : answer) {
-            if (num < Math.abs(min)) {
+            if (num < min) {
                 min = num;
             }
         }
         
         for (int i=0; i<answer.length; i++) {
-            if (min+n == array[i]) {
+            if (min == answer[i]) {
                 return array[i];
             }
         }
-            
-        return min+n;
+        
+        return 0;
     }
 }
